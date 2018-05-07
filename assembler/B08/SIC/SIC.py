ea83bcdc5898d4c2eda8dcc4c5294c03a3163fd8
YURU <rubystage@gmail.com>
2018-05-07 16:23:19 +0800

    no message

--- assembler/B08/SIC/SIC.py
+++ assembler/B08/SIC/SIC.py
@@ -7,14 +7,10 @@ Created on Mon Apr 30 13:24:11 2018
 import math
 
 sic=[]  #程式碼儲存
-op={'ADD':['18',3],'ADDR':['90',2],'AND':['40',3],'CLEAR':['4',2],'COMP':['28',3],'DIV':['24',3],'DIVR':['9C',2],
-    'FIX':['C4',1],'J':['3C',3],'JEQ':['30',3],'JGT':['34',3],'JLT':['38',3],'JSUB':['48',3],
-    'LDA':['00',3],'LDB':['68',3],'LDCH':['50',3],'LDL':['8',3],'LDS':['6C',3],'LDT':['74',3],'LDX':['4',3],
-    'MUL':['20',3],'MULF':['60',3],'MULR':['98',2],'OR':['44',3],'RMO':['AC',2],'RSUB':['4C',3],
-    'SIO':['F0',1],'SSK':['EC',3],'STA':['0C',3],'STB':['78',3],'STCH':['54',3],
-    'STF':['80',3],'STI':['D4',3],'STL':['14',3],'STS':['7C',3],'STSW':['E8',3],
-    'STT':['84',3],'STX':['10',3],'SUB':['1C',3],'SUBF':['5C',3],'SUBR':['94',2],'SVC':['B0',2],
-    'TD':['E0',3],'TIO':['F8',1],'TIX':['2C',3],'TIXR':['B8',2],'WD':['DC',3]}
+op={'ADD':['18',3],'AND':['40',3],'COMP':['28',3],'DIV':['24',3],'FIX':['C4',1],
+    'J':['3C',3],'JEQ':['30',3],'JGT':['34',3],'JLT':['38',3],'JSUB':['48',3],
+    'LDA':['00',3],'LDCH':['50',3],'LDL':['8',3],'LDX':['4',3],'MUL':['20',3],'OR':['44',3],'RD':['D8',3],'RSUB':['4C',3],
+    'STA':['0C',3],'STCH':['54',3],'STL':['14',3],'STX':['10',3],'SUB':['1C',3],'TD':['E0',3],'TIX':['2C',3],'WD':['DC',3]}
 sym={}  #SYMTAB
 locctr=0
 
@@ -102,7 +98,7 @@ for code in sic:  #輸出LISFILE
 print('\n輸出之OBJFILE: ')
 length=str(hex(sic[-1][0][0]-sic[0][0][0]))[2:].zfill(6).upper()
 print('H'+'{0:<6}'.format(sic[0][1])+sic[0][3].zfill(6)+length)
-objfile.write('H'+'{0:<6}'.format(sic[0][1])+sic[0][3].zfill(6)+length)
+objfile.write('H'+'{0:<6}'.format(sic[0][1])+sic[0][3].zfill(6)+length+'\n')
 
 objf=[]
 l=-1
@@ -110,27 +106,29 @@ t=''
 for i,code in enumerate(sic):
     if code[0]=='.' or code[4]=='':  #省略註解行 及 RESW RESB
         continue
-    if l==-2:
+    if code[2]=='END':
+        if t!='':
+            objfile.write(str(hex(l))[2:].zfill(2).upper()+t+'\n')
+            print(str(hex(l))[2:].zfill(2).upper()+t)
+        objfile.write('E'+sym[code[3]].zfill(6)+'\n')
         print('E'+sym[code[3]].zfill(6))
         break
     elif l==-1:
+        objfile.write('T'+code[0][1].zfill(6))
         print('T'+code[0][1].zfill(6),end='')
         t+=code[4]
         l+=1+math.ceil(len(code[4])/2)
-    elif sic[i+1][2]=='END':
-        print(str(hex(l))[2:].zfill(2).upper()+t)
-        l=-2
     elif l<=30:
         l+=math.ceil(len(code[4])/2)
         t+=code[4]
-        while sic[i+1][0]=='.':
-            i=i+1
-        if math.ceil(len(sic[i+1][4])/2)+l>30 or sic[i+1][4]=='':
-            print(str(hex(l))[2:].zfill(2).upper()+t)
-            t=''
-            l=-1
-           
-            
+    while sic[i+1][0]=='.':
+        i=i+1
+    if math.ceil(len(sic[i+1][4])/2)+l>30 or sic[i+1][4]=='':
+        objfile.write(str(hex(l))[2:].zfill(2).upper()+t+'\n')
+        print(str(hex(l))[2:].zfill(2).upper()+t)
+        t=''
+        l=-1
+        
  
 srcfile.close()
 lisfile.close()
